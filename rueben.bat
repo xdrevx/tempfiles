206204453ea78d45c0e6a82990bd514febe08773
xdrevx <148322704+xdrevx@users.noreply.github.com>
2026-07-11 17:16:20 +0800

    Update rueben.bat

--- rueben.bat
+++ rueben.bat
@@ -1,13 +1,13 @@
 @echo off
 chcp 65001 >nul
-title Rueben - Loading
+title Rueben - Launching (Reconfiguring Windows)
 goto one
 
 rem Credits to Rueben for the script
 rem Credits to online-fix.me for Minecraft generic fix method
 
 :example
-title Rueben - Injected into Minecraft and Windows
+title Rueben - Launching 
 echo.
 echo.
 echo.
@@ -44,13 +44,10 @@ timeout /t 5 >nul
 chcp 437 >nul
 powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; Add-Type -AssemblyName System.Drawing; $notify = New-Object System.Windows.Forms.NotifyIcon; $notify.Icon = [System.Drawing.SystemIcons]::Information; $notify.Visible = $true; $notify.ShowBalloonTip(0, 'Rueben', 'Minecraft.Windows.exe process 1 permanent unlock success', [System.Windows.Forms.ToolTipIcon]::None)}"
 chcp 65001 >nul
-timeout /t 10 >nul
-exit
+pause >nul
 
 :one
 chcp 437 >nul
-powershell.exe -Command "$wshell = New-Object -ComObject Wscript.Shell; $wshell.Popup('Allow Rueben to access and hack your PC?', 0, 'Rueben', 64)"
-chcp 65001 >nul
 taskkill /f /im explorer.exe
 cls
 echo.
