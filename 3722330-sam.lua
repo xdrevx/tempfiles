4bdf40a973d3c1c0986a3b5b121535864774d4ee
xdrevx <148322704+xdrevx@users.noreply.github.com>
2026-08-16 14:23:51 +0800

    Add files via upload

---
+++ 3722330-sam.lua
@@ -0,0 +1,9 @@
+-- Shift At​​‌‌​​‌‌​​‌‌​​​‌​‌‌​​​‌​​‌‌​​​​‌​​‌‌‌​​‌​​‌‌​‌​​​‌‌​​​‌‌​​‌‌‌​​​​‌‌​​​‌​​​‌‌​​‌‌​​‌‌​​​​​​‌‌​​​‌​‌‌​​‌​​​‌‌​​​‌‌​​‌‌​​‌‌​​‌‌​​‌​ Midnight
+-- AppID 3722330 | Generated on 2026-08-15 06:29 UTC | openlua.cloud
+-- Note: This game has multiplayer features
+
+-- Main Application
+addappid(3722330)
+
+-- Content Depots (1)
+addappid(3722331, 1, "241b88aeb27a250cbfff336e4e5ec6f28a60d136f4431f8bdcbf84a0506e5ed4") -- Shift At Midnight - Content
